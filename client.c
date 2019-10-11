a0ca6ffa19c26f9f42d1bfb944b342bd4516ca85
plum7ree <lomojiki@gmail.com>
2019-10-11 13:47:58 -0400

    raw algorithm

--- client.c
+++ client.c
@@ -1,175 +1,52 @@
-#include <fcntl.h>
-#include <sys/mman.h>
-#include <sys/types.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <errno.h>
-#include <string.h>
-#include <semaphore.h>
-#include <mqueue.h>
-
-#include "TinyFile.h"
 
 
-// link with -lrt -pthread
 
+void connectClient() {
+	//1. send message with sem_allow_msg, sem_msg_sent
 
-size_t fread_buf( void* ptr, size_t size, FILE* stream)
-{
-    return fread( ptr, 1, size, stream);
+}
+
+void sendFile() {
+	while(filesize) {
+		// 1. read a chunk from file
+		readChunk
+		sem_wait(sem_allow_msg)
+		sendMsg
+		sem_post(sem_msg_sent)
+
+		// 2. send a chunk to shared mem
+		sem_wait(sem_allow_transf)
+		sendToSM
+		sem_post(sem_modif)
+
+	}
+}
+
+void receiveFile() {
+	while(filesize) {
+		sem_wait(sem_modif_by_server)
+		receiveFromSM
+
+		sem_post(sem_modif_by_server)
+	}
 }
 
 
-size_t fwrite_buf( void const* ptr, size_t size, FILE* stream)
-{
-    return fwrite( ptr, 1, size, stream);
+int main() {
+
+	connectClient();
+
+	
+
+
+
+	readMsg()
+	filesize 
+	filenumber
+	
+		
+
+	
+
+
 }
-
-
-int
-main(int argc, char *argv[])
-{   
-    int r;
-    int fd;
-    size_t len = SEGSIZE;
-    char *addr;
-    char *buff = malloc(len);
-    /* Size of shared memory object */
-    FILE * pFile;
-    sem_t *sem, *sem_modif, *sem_allow_transf;
-
-
-    /****************************** msg queue ******************************************/
-    // int pid = getpid();
-    // printf("client pid: %d", pid);
-
-
-
-    // mqd_t mqfd = mq_open(MQPATH, O_WRONLY, 0666, NULL);
-    // if(mqfd == -1) {
-    //     perror("Child mq_open failure");
-    //     exit(0);
-    // }
-
-    // int PIDSIZE = 8;
-    // char *msgbuff = malloc(PIDSIZE);
-    // sprintf(msgbuff, "%d", pid);
-
-    // // mqd_t recvq = mq_open(msgbuff, O_WRONLY | O_CREAT, 0666, NULL); // client's pid becomes receving mq from server
-
-
-
-    // int status = mq_send(mqfd, msgbuff, MSGSIZE, 0);
-    // if (status == -1) {
-    //     perror("mq_send failure\n");
-    // }
-
-    // mq_close(mqfd);
-    // printf("Child process done\n");
-
-    // // mq_receive(recvq, msgbuff, 1000, 0);
-    // // mq_close(recvq);
-    
-
-    // return 0;
-    /****************************** msg queue ******************************************/
-
-    /****************************** File Transfer ******************************************/
-    int pid = getpid();
-    printf("client pid: %d\n", pid);
-
-
-
-    mqd_t mqfd = mq_open(MQPATH, O_WRONLY, 0666, NULL);
-    if(mqfd == -1) {
-        perror("Child mq_open failure");
-        exit(0);
-    }
-
-    int PIDSIZE = 8;
-    char *msgbuff = malloc(PIDSIZE);
-
-     pFile = fopen(filepath, "rb");
-    if (pFile==NULL) {
-        fputs ("File error",stderr); 
-        exit (1);
-    }
-    // obtain file size:
-    fseek (pFile , 0 , SEEK_END);
-    long fileSize = ftell (pFile);
-    sprintf(msgbuff, "%lu", fileSize);
-    rewind (pFile);
-    printf("file size: %lu\n", fileSize);
-
-    int status = mq_send(mqfd, msgbuff, MSGSIZE, 0);
-    if (status == -1) {
-        perror("mq_send failure\n");
-    }
-
-    mq_close(mqfd);
-
-    sem_modif = sem_open(SEMPATH2 , O_RDWR,S_IRUSR | S_IWUSR);
-    sem_allow_transf = sem_open(SEMPATH3 , O_RDWR,S_IRUSR | S_IWUSR);
-
-    if (sem == -1){
-        perror("sem_open");
-    }
-
-    fd = shm_open(SHMPATH, O_RDWR , 0);
-    if (fd == -1)
-        perror("shm_open");
-
-    if (ftruncate(fd, len) == -1)
-        perror("ftruncate failed");
-                                                /* Open existing object */
-    addr = mmap(NULL, len, PROT_WRITE, MAP_SHARED, fd, 0); 
-    if (addr == MAP_FAILED)
-            perror("mmap");
-    
-
-    printf("mmap addr: %x\n", addr);
-    fflush(stdout); // printf doesn't print exactly when is was called.
-
-    // int value = -2;
-    // sem_getvalue(sem_modif, &value);
-    // printf("sem val: %d", value);
-
-   
-
-    char *debugbuff = malloc(len);
-    long cumSize = 0;
-    while(1) {
-        sem_wait(sem_allow_transf);
-        printf("writen to buffer\n");
-
-        memset(addr, 0, len);
-        int res = fread_buf(addr, len, pFile);
-
-        cumSize += len;
-        
-        
-        r = sem_post(sem_modif);
-
-        if (cumSize >= fileSize) {
-            fclose(pFile);
-            break;
-        }
-        
-
-    }   
-
-
-    
-    munmap(addr, len);
-    shm_unlink(SHMPATH);
-    sem_unlink(SEMPATH);
-    sem_unlink(SEMPATH2);
-    sem_unlink(SEMPATH3);
-    if (close(fd) == -1)
-        perror("close"); /* 'fd' is no longer needed */
-    // exit(EXIT_SUCCESS);
-    /****************************** File Transfer ******************************************/
-}
-
-
--- server.c
+++ server.c
@@ -23,14 +23,6 @@ struct clone_arg {
     char msg[MSGSIZE];
 };
 
-int fd;
-int segsize = SEGSIZE;
-char *addr;
-struct stat sb;
-sem_t *sem, *sem_modif, *sem_allow_transf;
-FILE * pFile;
-int wpid;
-int status = 0;
 
 
 size_t fread_buf( void* ptr, size_t size, FILE* stream)
@@ -44,21 +36,57 @@ size_t fwrite_buf( void const* ptr, size_t size, FILE* stream)
     return fwrite( ptr, 1, size, stream);
 }
 
-void run_file_transfer (void *arg) {
-    char *msg = ((struct clone_arg *)arg)->msg;
-    // mqd_t clientq = mq_open(msgbuff, O_RDWR, 0666, &attr);
-    // volatile int i = 0;
-    // volatile int a = 2;
-    // volatile int b = 1;
-    // for (;i< 10000000; i++) {
-    //     a = a + b + 1;
-    // }
-    // printf("msg: %s, a:%d", msg, a);
-    // fflush(stdout);
+void clientHandler(void *arg) {
+    // char *msg = ((struct clone_arg *)arg)->msg;
+    sem_allow_transf = // sem only shared between one client and server
 
-    // 1. "/sem_client_pid" ex) /sem3771
-    // mq_open(/sem3771)
-    // 
+    
+    while()
+    {
+	    sem_wait(sem_global); // shared with all clients
+
+	    // 1. read request in message queue (file name, chunk size)
+	    sem_post(sem_allow_msg)
+	    sem_wait(sem_msg_sent)
+	    msg = readMsg()
+	    filenumber
+	    chunksize = CHUNKSIZE(msg);
+
+	    if(file_info_array[filenumber].filesize == 0) {
+	    	// if filenumber not exist in file_info_array:
+		    // 	first packet includes filenumber, chunksize, filesize
+		    // 	further packet includes filenumber, chunksize
+	    	file_info_array[filenumber].filesize = FILESIZE(msg);
+	    	file_info_array[filenumber].cumsize = 0;
+	    	chunksize = CHUNKSIZE(msg);
+	    	char *buffer = malloc(FILESIZE(msg));
+	    	file_info_array[filenumber].buffer = buffer;
+	    }
+	    sem_post(sem_msg_sent)
+
+
+	    sem_post(sem_allow_transf)
+	    sem_wait(sem_modif);
+	    char *temp_file_buffer =  file_info_array[filenumber].buffer + cumSize
+
+	    memcopy(temp_file_buffer, mmap_addr, chunksize);
+
+
+	    file_info_array[filenumber].cumsize += chunksize;
+	    totalsize = file_info_array[filenumber].filesize;
+
+	    if (cumSize >= totalsize) {
+    		break;
+    	}
+
+	}
+
+	compress();
+    
+
+    sem_wait(sem_global); // server should acquire this to write to shared memory
+
+
 
 
 
@@ -66,94 +94,42 @@ void run_file_transfer (void *arg) {
 }
 
 
-
-int
-main(int argc, char *argv[])
-{
-
-    /****************************** msg queue ******************************************/
-    // struct   timespec tm;
-    // clock_gettime(CLOCK_REALTIME, &tm);
-    // tm.tv_sec += 20;
-
-    // struct mq_attr attr;
-    // attr.mq_maxmsg = 4;
-    // attr.mq_msgsize = MSGSIZE;
-    // attr.mq_flags = 0;
-    // mqd_t mqfd = mq_open(MQPATH, O_RDWR | O_CREAT, 0666, &attr);
-    // char *msgbuff = malloc(MSGSIZE);
-
-    // // printf("server pid: %d\n", getpid());
-
-    // if(mqfd == -1) {
-    //     perror("Parent mq_open failure");
-    //     exit(0);
-    // }
+void connectClient(void *arg) {
+	// arg
+	// 1. sem_allow_transf : semaphore per client and server 
+	// 2. msg queue
+    int clone_pid = clone(clientHandler, (void *)child_stack, SIGCHLD, (void *)arg);
+}
 
 
-    // while(1) {
-    //     int status = mq_receive(mqfd, msgbuff, 1000, 0);
-    //     if (status == -1) {
-    //         perror("mq_receive failure\n");
-    //     }
-    //     int client_pid = atoi(msgbuff);
-    //     char *child_stack = malloc(STACKSIZE);
-    //     child_stack += STACKSIZE; //stack top, stack grows down
 
-    //     struct clone_arg *arg = (struct clone_arg *) malloc(sizeof(struct clone_arg));
-    //     memcpy(arg->msg, msgbuff, MSGSIZE);
+void initTinyServer(mqd_t *mqfd, char *mmap_addr, sem_t *sem_global) {
+	struct mq_attr *attr = malloc();
+    attr->mq_maxmsg = 4;
+    attr->mq_msgsize = MSGSIZE;
+    attr->mq_flags = 0;
+    *mqfd = mq_open(MQPATH, O_RDWR | O_CREAT, 0666, attr);
 
-
-        
-    // }
-
-        // while ((wpid = wait(&status)) > 0);
-
-
-        // mq_close(mqfd);
-        // mq_unlink(MQPATH);
-    
-    
-    // return 0;
-    /****************************** msg queue ******************************************/
-
-
-    /****************************** File Transfer ******************************************/
-
-     struct   timespec tm;
-    clock_gettime(CLOCK_REALTIME, &tm);
-    tm.tv_sec += 20;
-
-    struct mq_attr attr;
-    attr.mq_maxmsg = 4;
-    attr.mq_msgsize = MSGSIZE;
-    attr.mq_flags = 0;
-    mqd_t mqfd = mq_open(MQPATH, O_RDWR | O_CREAT, 0666, &attr);
-    char *msgbuff = malloc(MSGSIZE);
-
-    // printf("server pid: %d\n", getpid());
-
-    if(mqfd == -1) {
+    if(*mqfd == -1) {
         perror("Parent mq_open failure");
         exit(0);
     }
 
-    int status = mq_receive(mqfd, msgbuff, 1000, 0);
-    if (status == -1) {
-        perror("mq_receive failure\n");
-    }
-    long fileSize = (long) atoi(msgbuff);
-    printf("filesize: %lu\n", fileSize);
+    // int status = mq_receive(mqfd, msgbuff, 1000, 0);
+    // if (status == -1) {
+    //     perror("mq_receive failure\n");
+    // }
 
-    mq_close(mqfd);
-    mq_unlink(MQPATH);
+    // long fileSize = (long) atoi(msgbuff);
+    // printf("filesize: %lu\n", fileSize);
 
-    sem_modif = sem_open(SEMPATH2 , O_RDWR| O_CREAT,S_IRUSR | S_IWUSR);
-    sem_allow_transf = sem_open(SEMPATH3 , O_RDWR| O_CREAT,S_IRUSR | S_IWUSR);
+    // mq_close(mqfd);
+    // mq_unlink(MQPATH);
+
+    sem_global = sem_open(SEMPATH_GLOBAL , O_RDWR| O_CREAT,S_IRUSR | S_IWUSR);
     
 
-    sem_init(sem_modif, 1, 0);
-    sem_init(sem_allow_transf, 1, 1);
+    sem_init(sem_global, 1, 0);
 
     fd = shm_open(SHMPATH, O_RDWR| O_CREAT, S_IRUSR | S_IWUSR); /* Open existing object */ 
     
@@ -164,79 +140,42 @@ main(int argc, char *argv[])
     if (fstat(fd, &sb) == -1)
         perror("fstat");
 
-    addr = mmap(NULL, segsize, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0); 
-    // printf("mmap addr: %x\n", addr);
+    mmap_addr = mmap(NULL, segsize, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0); 
+    // printf("mmap_addr: %x\n", mmap_addr);
     // // fflush(stdout);
 
-    if (addr == MAP_FAILED)
+    if (mmap_addr == MAP_FAILED)
         perror("mmap");
-    
-    // pFile = fopen("result.txt", "wb+");
-
-    long cumSize = 0;
-    // long fileSize = 12; // through message queue
-    char *debugbuff = malloc(segsize);
-    char *tempFileBuffer = malloc(ONE_MB/2);
-
-    /************************************ Snappy Setting *************************************************/
-    char *outbuffer = xmalloc(snappy_max_compressed_length(fileSize));
-    char *outbuffer2 = malloc(ONE_MB/2);
-    size_t outlen = -1;
-    struct snappy_env env;
-    snappy_init_env(&env);
-    /*****************************************************************************************************/
-    
-    while(1) {
-        sem_wait(sem_modif);
-        memcpy(debugbuff, addr, segsize);
-        printf("received file seg: %s\n", debugbuff);
-        // fwrite_buf(addr, segsize, pFile);
-        memcpy(tempFileBuffer + cumSize, addr, segsize);
-        // msync(addr, segsize, MS_SYNC);
-        cumSize += segsize;
-        
-
-        if (cumSize >= fileSize) {
-            // fclose(pFile);
-            // TODO: compress file
-            // TODO: notify client it's done
-            // mq_send()
-            int err = snappy_compress(&env, tempFileBuffer, fileSize, outbuffer, &outlen);       
-            if (err) {
-                printf("compression of failed:\n");
-            }
-            printf("compression done. outlen: %d\n", outlen);
-            fflush(stdout);
-
-            err = snappy_uncompress(outbuffer, outlen, outbuffer2);
-            if (err) {
-                printf("uncompression failed\n");
-                break;
-            }
-            if (memcmp(outbuffer2, tempFileBuffer, fileSize)) {
-                printf("failed! compression\n");
-                
-            } 
-            else {
-                printf("%s OK!\n");
-            }
-
-            break;
 
 
-        }
-
-        sem_post(sem_allow_transf);
-
-        
-    }
-
-    // printf("file transfered succedded!\n");
-    // munmap(addr, segsize);
-    // shm_unlink(SHMPATH);
-    // sem_unlink(SEMPATH);
-    // sem_unlink(SEMPATH2);
-    // sem_unlink(SEMPATH3);
-    // exit(EXIT_SUCCESS);
-    /****************************** File Transfer ******************************************/
 }
+
+int
+main(int argc, char *argv[])
+{
+	int fd;
+	int segsize = SEGSIZE;
+	char *addr;
+	struct stat sb;
+	sem_t *sem_global, *sem_modif;
+	FILE * pFile;
+	int wpid;
+	int status = 0;
+	
+	mqd_t *mqfd = malloc();
+
+
+	initTinyServer(attr, mmap_addr, sem_global);
+
+	while(1) {
+		//1. check mq for new client creation
+
+		//2. create thread for that client (clone)
+		connectClient();
+	}
+
+
+
+
+}
+
