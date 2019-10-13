ef9e5bcf8e5c4a7c5aa2eb3b77bdc2430d96112c
Quang Van Huynh <quang.huynh@gatech.edu>
2019-10-13 08:07:09 -0400

    Added yaml parser

---
+++ parse.c
@@ -0,0 +1,75 @@
+#include <yaml.h>
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#ifdef NDEBUG
+#undef NDEBUG
+#endif
+#include <assert.h>
+
+int
+main(int argc, char *argv[])
+{
+    int number;
+
+    if (argc < 2) {
+        printf("Usage: %s file1.yaml ...\n", argv[0]);
+        return 0;
+    }
+
+    for (number = 1; number < argc; number ++)
+    {
+        FILE *file;
+        yaml_parser_t parser;
+        yaml_event_t event;
+        int done = 0;
+        int count = 0;
+        int error = 0;
+        char* value;
+        int expecting_filename = 0;
+
+        printf("[%d] Parsing '%s':\n", number, argv[number]);
+        fflush(stdout);
+
+        file = fopen(argv[number], "rb");
+        assert(file);
+
+        assert(yaml_parser_initialize(&parser));
+
+        yaml_parser_set_input_file(&parser, file);
+
+        while (!done)
+        {
+            if (!yaml_parser_parse(&parser, &event)) {
+                error = 1;
+                break;
+            }
+
+
+            if (event.type == YAML_SCALAR_EVENT) {
+              value = event.data.scalar.value;
+              if (expecting_filename) {
+                printf("%s\n", value);
+                expecting_filename = 0;
+              }
+              if (strcmp(value, "FILE_TYPE") == 0) {
+                expecting_filename = 1;
+              }
+            }
+            done = (event.type == YAML_STREAM_END_EVENT);
+
+            yaml_event_delete(&event);
+
+            count++;
+        }
+
+        yaml_parser_delete(&parser);
+
+        assert(!fclose(file));
+
+        /* printf("%s (%d events)\n", (error ? "FAILURE" : "SUCCESS"), count); */
+    }
+
+    return 0;
+}
