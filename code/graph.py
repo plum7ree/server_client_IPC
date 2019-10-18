2fd72e90a3ff87286a0494af82c913f4234f5faa
plum7ree <lomojiki@gmail.com>
2019-10-18 01:40:19 -0400

    graph.py, makefile, tester.sh modified

--- code/graph.py
+++ code/graph.py
@@ -1,21 +1,46 @@
 import matplotlib.pyplot as plt
 
-dic = { 1.0 : "some custom text"}
-labels = [[], []]
+
+for i in [1,3,5] :
+	xaxis = [x for x in range(1,10)]
+	names = []
+	values = []
+	with open("../output/stress_testing_sync_" + str(i) + "_segment.csv", "r") as f:
+	    for line in f:
+	        cst, sms_size = line.split(",")
+	        names.append(sms_size.strip());
+	        values.append(float(cst));
+	print(names)
+	print(values)
+	plt.figure()
+	plt.bar(xaxis, values, align="center")
+	plt.xticks(xaxis, names)
 
 
-data = [[], []]
-with open("stats.csv", "r") as f:
-    for line in f:
-        cst, sms_size = line.split(",")
-        data[0] += [int(sms_size)]
-        data[1] += [float(cst)]
-        labels[0] += [int(sms_size)]
-        labels[1] += [sms_size]
+	plt.savefig("../output/stress_testing_sync_" + str(i) + "_segment.png")
+	f.close()
 
-plt.plot(*data)
-plt.xlabel('cst')
-plt.ylabel('segment_size')
-plt.xticks(*labels)
-plt.grid(True)
-plt.show()
+
+# dic = { 1.0 : "some custom text"}
+# labels = [[], []]
+
+
+# data = [[], []]
+# with open("../output/stress_testing_sync_1_segment.csv", "r") as f:
+#     for line in f:
+#         cst, sms_size = line.split(",")
+#         data[0] += [int(sms_size)]
+#         data[1] += [float(cst)]
+#         labels[0] += [int(sms_size)]
+#         labels[1] += [sms_size]
+
+
+# plt.bar(labels[0])
+# plt.plot(*data)
+# plt.xlabel('cst')
+# plt.ylabel('segment_size')
+# plt.xticks(*labels)
+# plt.grid(True)
+# # plt.show()
+
+# plt.savefig('../output/stress_testing_sync_1_segment.png')
